62f71c42b9ea247e523aeb905f8151feba9688bd
youn9k <ace_lephant@naver.com>
2024-01-05 23:30:42 +0900

    💄 :: Liquid 애니메이션 뷰 추가

---
+++ IntervalTimer/Sources/Views/Components/LiquidView.swift
@@ -0,0 +1,25 @@
+//
+//  SlimeView.swift
+//  IntervalTimer
+//
+//  Created by YoungK on 1/5/24.
+//
+
+import SwiftUI
+import Liquid
+
+struct LiquidView: View {
+    var color: Color = .blue
+    var body: some View {
+        Liquid(samples: 6, period: 1)
+            .foregroundStyle(color)
+    }
+}
+
+#Preview {
+    VStack {
+        LiquidView(color: .timerBlue)
+            .frame(width: 300, height: 200)
+    }.frame(width: APP_WIDTH(), height: APP_HEIGHT())
+    
+}
