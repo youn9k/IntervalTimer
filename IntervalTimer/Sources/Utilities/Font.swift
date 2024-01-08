521ec37bb5955c534f500f58555e062c9ef382c2
youn9k <ace_lephant@naver.com>
2024-01-08 19:27:44 +0900

    ✨ :: 폰트 weight 적용되도록 수정

--- IntervalTimer/Sources/Utilities/Font.swift
+++ IntervalTimer/Sources/Utilities/Font.swift
@@ -14,24 +14,24 @@ public extension Font {
         case medium = "Inter-Medium"
         case bold = "Inter-Bold"
         
-        func font(size: CGFloat) -> Font {
+        func font(size: CGFloat, weight: Font.Weight) -> Font {
             if let uiFont = UIFont(name: self.rawValue, size: size) {
-                return Font(uiFont)
+                return Font(uiFont).weight(weight)
             } else {
-                return Font.system(size: size)
+                return Font.system(size: size).weight(weight)
             }
         }
 
         static func light(size: CGFloat) -> Font {
-            return TimerFont.light.font(size: size)
+            return TimerFont.light.font(size: size, weight: .light)
         }
         
         static func medium(size: CGFloat) -> Font {
-            return TimerFont.medium.font(size: size)
+            return TimerFont.medium.font(size: size, weight: .medium)
         }
         
         static func bold(size: CGFloat) -> Font {
-            return TimerFont.bold.font(size: size)
+            return TimerFont.bold.font(size: size, weight: .bold)
         }
     }
 
