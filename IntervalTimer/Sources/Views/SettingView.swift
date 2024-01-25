e13164f5f0ceac9ee2bc0bc86c69b259c1040932
youn9k <ace_lephant@naver.com>
2024-01-25 10:04:41 +0900

    🔧 :: 아이콘 리소스 추가

---
+++ IntervalTimer/Sources/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  IntervalTimer
+//
+//  Created by YoungK on 1/12/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
