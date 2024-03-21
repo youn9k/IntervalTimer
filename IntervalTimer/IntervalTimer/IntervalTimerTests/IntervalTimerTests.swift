87f68acc341cacecff2f30be0d45bb96397c833d
youn9k <ace_lephant@naver.com>
2024-03-21 19:35:07 +0900

    🐛 :: 앱스토어 업로드가 안되던 문제 수정
    Missing CFBundleIconName 오류가 뜨는데 이미 앱아이콘, plist 등등 관련 설정은 모두 해 준 상태. 해결해보려고 삽질하다가 모두 실패하고 결국 최후의 보루로 프로젝트를 새로파서 소스 코드를 모두 옮김

---
+++ IntervalTimer/IntervalTimer/IntervalTimerTests/IntervalTimerTests.swift
@@ -0,0 +1,21 @@
+//
+//  IntervalTimerTests.swift
+//  IntervalTimerTests
+//
+//  Created by YoungK on 12/16/23.
+//
+
+import XCTest
+@testable import IntervalTimer
+
+final class IntervalTimerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
